6bb6ef6eaba4c7306af7bf4678178052c9f16bf3
Your Name <23aw0103@jec.ac.jp>
2024-11-06 02:39:52 +0900

    TodoAppで取得したデータをコンソール出力

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import { getAllTodos } from '../utils/supabaseFunctions'
 import TodoList from '@/components/TodoList';
 
 const TodoApp: React.FC = () => {
+
+  const [todos, setTodos] = useState<any>([]);
+
+  useEffect(() => {
+    const getTodos = async () => {
+      const todos = await getAllTodos();
+      setTodos(todos);
+      console.log(todos);
+    }
+    getTodos();
+  },[]);
+
   return (
     <section className='text-center mb-2 text-2xl font-medium'>
       <h3 className=''>Supabese Todo App</h3>
@@ -9,6 +24,7 @@ const TodoApp: React.FC = () => {
         <input type="text" className='shadow-lg p-1 outline-none'/>
         <button className='shadow-md border-2 px-1 py-1 rounded-lg bg-green-200'>Add</button>
       </form>
+      <TodoList/>
     </section>
   );
 }
