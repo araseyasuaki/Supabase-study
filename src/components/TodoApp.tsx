f6667f125fc7f4baa935709fb9b8c08c517e0c81
Your Name <23aw0103@jec.ac.jp>
2024-11-06 02:59:23 +0900

    テスト3

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -1,32 +1,32 @@
-// 'use client';
+'use client';
 
-// import React, { useEffect, useState } from 'react';
-// // import { getAllTodos } from '../utils/supabaseFunctions'
+import React, { useEffect, useState } from 'react';
+// import { getAllTodos } from '../utils/supabaseFunctions'
 // import TodoList from './TodoList';
 
-// const TodoApp: React.FC = () => {
+const TodoApp: React.FC = () => {
 
-//   // const [todos, setTodos] = useState<any>([]);
+  // const [todos, setTodos] = useState<any>([]);
 
-//   // useEffect(() => {
-//   //   const getTodos = async () => {
-//   //     const todos = await getAllTodos();
-//   //     setTodos(todos);
-//   //     // console.log(todos);
-//   //   }
-//   //   getTodos();
-//   // },[]);
+  // useEffect(() => {
+  //   const getTodos = async () => {
+  //     const todos = await getAllTodos();
+  //     setTodos(todos);
+  //     // console.log(todos);
+  //   }
+  //   getTodos();
+  // },[]);
 
-//   return (
-//     <section className='text-center mb-2 text-2xl font-medium'>
-//       <h3 className=''>Supabese Todo App</h3>
-//       <form>
-//         <input type="text" className='shadow-lg p-1 outline-none'/>
-//         <button className='shadow-md border-2 px-1 py-1 rounded-lg bg-green-200'>Add</button>
-//       </form>
-//       <TodoList/>
-//     </section>
-//   );
-// }
+  return (
+    <section className='text-center mb-2 text-2xl font-medium'>
+      <h3 className=''>Supabese Todo App</h3>
+      <form>
+        <input type="text" className='shadow-lg p-1 outline-none'/>
+        <button className='shadow-md border-2 px-1 py-1 rounded-lg bg-green-200'>Add</button>
+      </form>
+      {/* <TodoList/> */}
+    </section>
+  );
+}
 
-// export default TodoApp;
+export default TodoApp;
