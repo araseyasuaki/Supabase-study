23be6c55ed5dcc4dfffcf50b2279aad5414b1028
Your Name <23aw0103@jec.ac.jp>
2024-11-06 02:20:54 +0900

    TodoAppの入力フォール作成

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import TodoList from '@/components/TodoList';
 
 const TodoApp: React.FC = () => {
   return (
-    <div>TodoApp</div>
+    <section className='text-center mb-2 text-2xl font-medium'>
+      <h3 className=''>Supabese Todo App</h3>
+      <form>
+        <input type="text" className='shadow-lg p-1 outline-none'/>
+        <button className='shadow-md border-2 px-1 py-1 rounded-lg bg-green-200'>Add</button>
+      </form>
+    </section>
   );
 }
 
