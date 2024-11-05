36966447386e933b39751c5719d87ee6198bb775
Your Name <23aw0103@jec.ac.jp>
2024-11-06 02:39:17 +0900

    supabaseのデータ取得でtodosのdataオブジェクト指定

--- src/utils/supabaseFunctions.ts
+++ src/utils/supabaseFunctions.ts
@@ -2,5 +2,5 @@ import { supabase } from '../utils/supabase';
 
 export const getAllTodos = async () => {
   const todos = await supabase.from('todo').select('*');
-  return todos;
+  return todos.data;
 }
