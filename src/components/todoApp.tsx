5d7ebb326b1b269db0b3ab83b21a79993b3730be
Your Name <23aw0103@jec.ac.jp>
2024-11-08 03:11:07 +0900

    不要物削除

--- src/components/todoApp.tsx
+++ src/components/todoApp.tsx
@@ -28,14 +28,7 @@ const TodoApp: React.FC = () => {
         <input type="text" className='shadow-lg p-1 outline-none'/>
         <button className='shadow-md border-2 px-1 py-1 rounded-lg bg-green-200'>Add</button>
       </form>
-      <TodoList/>
-      <div>
-        {todos.map((todo) => (
-          <div key={todo.id} className='p-2 border-b'>
-            {todo.title} {/* Todoのタイトルを表示 */}
-          </div>
-        ))}
-      </div>
+      <TodoList todos={todos}/>
     </section>
   );
 }
