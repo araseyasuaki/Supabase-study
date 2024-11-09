8b8da112679158e9353a7f7a1e7a5d1a46e80a86
Your Name <23aw0103@jec.ac.jp>
2024-11-09 14:20:56 +0900

    addボタンでデータを追加。これにより、inputタグのリセットとデータ取得関数の呼び出しを追加

--- src/components/todoApp.tsx
+++ src/components/todoApp.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { getAllTodos } from '../utils//supabaseFunctions'
+import { getAllTodos, addTodo } from '../utils//supabaseFunctions'
 import TodoList from './todoList';
 
 interface Todo {
@@ -12,20 +12,33 @@ interface Todo {
 const TodoApp: React.FC = () => {
 
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [title, setTitle] = useState<string>('');
 
   useEffect(() => {
     const getTodos = async () => {
       const todos = await getAllTodos();
-      setTodos(todos || []);
+      setTodos(todos ?? []);
     };
     getTodos();
   }, []);
 
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    if (title === "") return;
+
+    await addTodo(title);
+    let todos = await getAllTodos();
+    setTodos(todos ?? []);
+
+    setTitle("");
+  };
+
   return (
     <section className='text-center mb-2 text-2xl font-medium'>
-      <h3 className=''>Supabese Todo App</h3>
-      <form>
-        <input type="text" className='shadow-lg p-1 outline-none'/>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input type="text" className='shadow-lg p-1 outline-none' onChange={(e) => setTitle(e.target.value)} value={title}/>
         <button className='shadow-md border-2 px-1 py-1 rounded-lg bg-green-200'>Add</button>
       </form>
       <TodoList todos={todos}/>
